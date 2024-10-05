58993e21cb2b0814f257e9d0ee5514eb732ed18f
u-kai <76635578+u-kai@users.noreply.github.com>
2024-10-05 11:04:10 +0900

    add start and stop

--- src/index.js
+++ src/index.js
@@ -1,21 +1,53 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const WAVE_COLOR = "rgba(160,192,207,1)";
-
-// click したら徐々に止まってほしくて、最後は一直線になるようにしてほしい
-// 止まった後に再度クリックすると再開するようにしたい
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let waveHeight = 50; // 波の高さ
-let waveLength = 0.002; // 波の長さ
-let waveSpeed = 0.05; // 波の速度
-let phase = 0;
-let onStop = false;
+class WaveMotion {
+  constructor() {
+    this.isStop = false;
+  }
+  toggle() {
+    this.isStop = !this.isStop;
+  }
+}
+
+class WaveState {
+  constructor(
+    height = 50,
+    length = 0.002,
+    speed = 0.05,
+    phase = 0,
+    motion = new WaveMotion(),
+    color = "rgba(160,192,207,1)"
+  ) {
+    this.height = height;
+    this.maxHeight = height;
+    this.length = length;
+    this.speed = speed;
+    this.phase = phase;
+    this.motion = motion;
+    this.color = color;
+  }
+  update() {
+    if (this.motion.isStop) {
+      this.height *= 0.99;
+    }
+    if (!this.motion.isStop && this.height < this.maxHeight) {
+      //if (this.height === 0) {
+      //  this.height = 10;
+      //}
+      this.height *= 1.01;
+    }
+    this.phase += this.speed;
+  }
+  toggleMotion() {
+    this.motion.toggle();
+  }
+}
+
+const state = new WaveState();
 
 function drawWave() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -25,7 +57,7 @@ function drawWave() {
   ctx.moveTo(0, canvas.height / 2);
 
   for (let x = 0; x < canvas.width; x++) {
-    const y = waveHeight * Math.sin(x * waveLength + phase) + canvas.height / 2;
+    const y = calcPixelIndex(state, x, canvas.height / 2);
     ctx.lineTo(x, y);
   }
 
@@ -34,38 +66,26 @@ function drawWave() {
   ctx.closePath();
 
   // 波の塗りつぶし
-  ctx.fillStyle = WAVE_COLOR; // 外部で定義された色を使用
+  ctx.fillStyle = state.color;
   ctx.fill();
 
   // 波の淵（境界線）を白色で描画
   ctx.strokeStyle = "white"; // 淵の色
   ctx.lineWidth = 2; // 淵の太さ
   ctx.stroke();
-  phase += waveSpeed; // フェーズを進める
-  if (onStop) {
-    waveHeight *= 0.99; // 徐々に減速
-  }
 
-  // アニメーションが実行中の場合
-  // if (isAnimating) {
-  //   phase += waveSpeed; // フェーズを進める
-  //   waveSpeed *= 0.99; // 徐々に減速
-
-  //   // 一定の速度以下になったら一直線にする
-  //   if (Math.abs(waveSpeed) < 0.001) {
-  //     isAnimating = false; // フェーズの動きを止める
-  //     isStopped = true; // 波を一直線にする
-  //     waveSpeed = 0; // 完全に0にする
-  //   }
-  // }
-
+  state.update();
   requestAnimationFrame(drawWave);
 }
 
 // クリックイベントを設定
 canvas.addEventListener("click", () => {
   console.log("click");
-  onStop = !onStop;
+  state.toggleMotion();
 });
 
 drawWave();
+
+function calcPixelIndex(state, x, baseHeight) {
+  return state.height * Math.sin(x * state.length + state.phase) + baseHeight;
+}
